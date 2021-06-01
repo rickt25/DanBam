07d042f85204e422741a983f45a267425978b031
rickt25 <derricktio36@gmail.com>
2021-06-01 22:14:11 +0700

    navbar dropdown

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-$(function () {
+$(() => {
     $(document).scroll(function () {
         $("nav").toggleClass('scrolled', $(this).scrollTop() > 0);
     });
 
+    $("nav").hover(() => {
+        if($(this).scrollTop() == 0){
+            $("nav").toggleClass('scrolled');
+        }
+    })
+
+    $(".dropdown-btn").hover(() => {
+        $(".dropdown").toggleClass('show');
+    })
+
     $(".hamburger").click((e) => {
         $(".links").toggleClass("dropdown");
+        $(".hamburger span").toggleClass("white");
         e.preventDefault();
     });
 });
